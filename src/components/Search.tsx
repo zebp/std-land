d7c9abfe217d0cea418d20e959f65e322a29aecd
Zeb Piasecki <zeb@zebulon.dev>
2021-06-01 11:53:11 -0400

    docs: document things

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -16,6 +16,9 @@ import styles from './Search.module.css';
 interface SearchBoxProps {
   value: string;
   onChange: (text: string) => void;
+  /**
+   * Sends a message to the search container to manipulate the selected result.
+   */
   sendMessage: (message: Message) => void;
 }
 
@@ -49,26 +52,38 @@ interface ResultProps {
   item: SearchItem;
   selected: boolean;
   index: number;
+  /**
+   * Sends a message to the search container to manipulate the selected result.
+   */
   sendMessage: (message: Message) => void;
 }
 
+/**
+ * Goes to the item's source in the Deno standard library.
+ * @param item an item from the Deno stdlib.
+ */
 function goToItem(item: SearchItem) {
   let url = `https://deno.land/std/${item.path}`;
   if (item.lineNumber) url += `#L${item.lineNumber}`;
   window.location.href = url;
 }
 
+/// A component for search results.
 function Result({ item, selected, index, sendMessage }: ResultProps) {
   const [isHovering, hoveringProps] = useHover();
   useEffect(() => {
     if (isHovering) {
+      // Sends a message to the search container when we hover over the result to select this item.
       sendMessage({ type: `select`, index });
     }
   }, [isHovering]);
 
+  // The CSS classes to apply to the result container depending on if the result is selected.
   const classes = selected
     ? `${styles.selected} ${styles.result}`
     : styles.result;
+
+  // The icon element that we will render along side the name and path of the result and it's color.
   const [IconElem, iconColor] = match(item.type)
     .with(DenoItemType.Class, () => [VscSymbolClass, `#32a852`])
     .with(DenoItemType.Function, () => [VscSymbolMethod, `#a468bf`])
@@ -92,7 +107,15 @@ export interface SearchProps {
   results?: SearchItem[];
 }
 
+/**
+ * Message types that don't have any corresponding data, we need this as it's own type alias so we
+ * can refer to it with a cast in the {@link SearchBox} component.
+ */
 type SimpleMessageType = 'up' | 'down' | 'cancel' | 'go';
+
+/**
+ * Messages used to control the selected elements of the search results.
+ */
 type Message = { type: SimpleMessageType } | { type: 'select'; index: number };
 
 export default function Search({
@@ -108,7 +131,8 @@ export default function Search({
       (selection !== undefined ? selection + offset : 0) % results.length,
     );
 
-  const process = useCallback(
+  // Handles messages related to setting the selected item.
+  const handleMessage = useCallback(
     (message: Message) => {
       if (results.length === 0) {
         return;
@@ -145,6 +169,8 @@ export default function Search({
     }
   }, [query]);
 
+  // Hacky fix to get the height animation to work propertly.
+  // TODO: Make some of these magic values into constants.
   const elemHeight = query !== `` ? results.length * 3.5 : 0;
   const heightStyles = {
     height: `${4 + elemHeight}rem`,
@@ -152,20 +178,20 @@ export default function Search({
 
   return (
     <div id={styles.search} style={heightStyles}>
-      <SearchBox onChange={setQuery} value={query} sendMessage={process} />
-      {results.map((item, index) => {
-        const key = `${item.path}-${item.name}-${item.type}@${item.lineNumber}`;
-
-        return (
-          <Result
-            key={key}
-            item={item}
-            selected={index === selection}
-            sendMessage={process}
-            index={index}
-          />
-        );
-      })}
+      <SearchBox
+        onChange={setQuery}
+        value={query}
+        sendMessage={handleMessage}
+      />
+      {results.map((item, index) => (
+        <Result
+          key={`${item.path}-${item.name}-${item.type}@${item.lineNumber}`}
+          item={item}
+          selected={index === selection}
+          sendMessage={handleMessage}
+          index={index}
+        />
+      ))}
     </div>
   );
 }
--- src/search.ts
+++ src/search.ts
@@ -1,13 +1,7 @@
 import Fuse from 'fuse.js';
 import searchData from '../data.json';
 
-export { searchData };
-
-export const fuse = new Fuse(searchData as SearchItem[], {
-  includeScore: true,
-  keys: [`path`, `name`],
-});
-
+// Eslint bug?
 // eslint-disable-next-line no-shadow
 export enum DenoItemType {
   Class = `class`,
@@ -20,6 +14,17 @@ export enum DenoItemType {
   Namespace = `namespace`,
   TypeAlias = `typeAlias`,
 }
+
+/**
+ * The utility used to fuzzy search through all our data.
+ */
+export const fuse = new Fuse(searchData as SearchItem[], {
+  // Include the score in the results so we can later determine if it's enough of a match to
+  // automatically redirect.
+  includeScore: true,
+  // Try to fuzzy search against the `name` and `path` fields in the objects.
+  keys: [`path`, `name`],
+});
 
 export interface SearchItem {
   name: string;
